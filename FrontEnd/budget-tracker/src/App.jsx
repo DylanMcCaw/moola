2502dcfd91ee252179c4c6d1e6fed9ebe1b7262c
Dylan McCaw <dylanmccaw@hotmail.com>
2024-06-03 23:04:46 +0100

    Added api call to app layer

--- FrontEnd/budget-tracker/src/App.jsx
+++ FrontEnd/budget-tracker/src/App.jsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Loader } from '@mantine/core'; // Import Loader from Mantine
+import ExpenseApi from './api/ExpenseApi';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { MantineProvider } from '@mantine/core';
 import { Home, SavingPots } from './pages';
@@ -8,6 +11,26 @@ import '@mantine/core/styles.css';
 import './App.css';
 
 function App() {
+  const [expenses, setExpenses] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchExpenses = async () => {
+      try {
+        const userId = 0; // Replace with the actual user ID
+        const data = await ExpenseApi.getExpensesByUserId(userId);
+        setExpenses(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchExpenses();
+  }, []);
+
   return (
     <MantineProvider>
       <Router>
@@ -17,19 +40,29 @@ function App() {
           </header>
           <div className="content-container">
             <main className="main-content">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/savingpots" element={<SavingPots />} />
-                {/* 
-                <GuestRoute path="/login" element={<Auth key="login" />} />
-                <AuthRoute path="/settings" element={<Settings />} />
-                <AuthRoute path="/editor" element={<Editor />} />
-                <Route path="/editor/:slug" element={<Editor />} />
-                <Route path="/article/:slug" element={<Article />} />
-                <Route path="/profile/:username" element={<Profile />} />
-                <AuthRoute path="/@:username" element={<Profile />} />
-                */}
-              </Routes>
+              {/* Conditional rendering of loader */}
+              {loading && (
+                <div className="loader-container">
+                  <Loader size={75} color="#4333A1" />
+                </div>
+              )}
+              {/* Conditional rendering of error */}
+              {error && <div>Error: {error.message}</div>}
+              {!loading && !error && (
+                <Routes>
+                  <Route path="/" element={<Home expenses={expenses} />} />
+                  <Route path="/savingpots" element={<SavingPots />} />
+                  {/* 
+                  <GuestRoute path="/login" element={<Auth key="login" />} />
+                  <AuthRoute path="/settings" element={<Settings />} />
+                  <AuthRoute path="/editor" element={<Editor />} />
+                  <Route path="/editor/:slug" element={<Editor />} />
+                  <Route path="/article/:slug" element={<Article />} />
+                  <Route path="/profile/:username" element={<Profile />} />
+                  <AuthRoute path="/@:username" element={<Profile />} />
+                  */}
+                </Routes>
+              )}
             </main>
             <footer>
               <div className="container"></div>
--- FrontEnd/budget-tracker/src/pages/Home.jsx
+++ FrontEnd/budget-tracker/src/pages/Home.jsx
@@ -1,5 +1,3 @@
-// src/pages/Home.js
-
 import React, { useState, useEffect } from 'react';
 import { SavingsHomePageCard } from '../components/HomePage/SavingsHomePageCard';
 import { ExpensesHomePageCard } from '../components/HomePage/ExpensesHomePageCard';
@@ -7,29 +5,7 @@ import { IncomeHomePageCard } from '../components/HomePage/IncomeHomePageCard';
 import ExpenseApi from '../api/ExpenseApi';
 import './HomePageStyle.css';
 
-function Home() {
-  const [expenses, setExpenses] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchExpenses = async () => {
-      try {
-        const userId = 0; // Replace with the actual user ID
-        const data = await ExpenseApi.getExpensesByUserId(userId);
-        setExpenses(data);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchExpenses();
-  }, []);
-
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
+function Home({ expenses }) {
 
   return (
     <div className="home-page">
